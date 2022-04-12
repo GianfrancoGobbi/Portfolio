d3b459fa790e6d3e0ce05e66b2cd11de87669a2a
GianfrancoGobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-04-12 01:38:19 -0300

    Add files via upload

---
+++ proyecto/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HOLA SOY</h1>
+      <h2>GIANFRANCO</h2>
+    </div>
+  );
+}
+
+export default App;
