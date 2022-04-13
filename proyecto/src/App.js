ba795b6a516854cf31c7f2714d114e67a4832733
GianfrancoGobbi <gianfrancogobbi@outlook.com>
2022-04-13 10:46:39 -0300

    complete

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -6,8 +6,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>HOLA SOY</h1>
-      <h2>GIANFRANCO</h2>
+      <div className="contenedorHello">
+
+        <h1 className="h1">Hola, soy</h1>
+        <h2 className="h2">GIANFRANCO GOBBI</h2>
+        <img className="imgcolor" src={'https://media.istockphoto.com/photos/explosion-of-colored-powder-on-black-background-picture-id1192236158?k=20&m=1192236158&s=170667a&w=0&h=2LyexYGXdOqml_vBOOF4OicGuDp4K5ifut7xTjB9lJA='} ></img>
+      </div>
     </div>
   );
 }
