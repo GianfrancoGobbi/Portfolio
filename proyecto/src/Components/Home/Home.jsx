58f2a793f3f8d54a3ac3d2601fdf50a59bd15c51
GianfrancoGobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2022-04-16 00:17:20 -0300

    tecnologias

--- proyecto/src/Components/Home/Home.jsx
+++ proyecto/src/Components/Home/Home.jsx
@@ -26,11 +26,34 @@ export function Home() {
                 <img src={FondoGiant} className="imgFondo" alt="Fondo" />
             </div>
 
-            <h3 className="description">Mi nombre es Gianfranco, soy de Argentina.
-                Soy diseñador gráfico y Full Stack developer graduado
-                en Henry. Me adapto facilmente a los equipos de trabajo, autodidacta
-                y en busca de conocer mis límites.</h3>
+            <h3 className="description">Mi nombre es Gianfranco Gobbi, nací en Argentina.
+                Soy diseñador gráfico senior y Full Stack developer junior graduado
+                en Henry. Actualmente me desarrollo en la sistematización de ventas en línea.
+                Me gusta el trabajo en equipo, autodidacta
+                y siempre en busca de nuevos desafios para enfrentar.
+                <br />
+                Mis hobbies son los deportes, la comida y los videojuegos.
+            </h3>
+       
             <img className='imgprueba' src={'https://www.xtrafondos.com/wallpapers/resoluciones/19/explosion-polvo-y-humo-de-colores_3840x2160_3008.jpg'} alt='Explosion'></img>
+
+            <div>
+                <h2 className='h2'>Tecnologías incorporadas</h2>
+                <ul className='lista_tecnologias'>
+                    <li className='tecnologias'>Javascrip</li>
+                    <li className='tecnologias'>Html</li>
+                    <li className='tecnologias'>React</li>
+                    <li className='tecnologias'>Redux</li>
+                    <li className='tecnologias'>Illustrator</li>
+                    <li className='tecnologias'>Photoshop</li>
+                    <li className='tecnologias'>3D MAX</li>
+                    <li className='tecnologias'>Premiere</li>
+                    <li className='tecnologias'>After Effects</li>
+                </ul>
+            </div>
+
+
+
             <img className='imgprueba' src={'https://www.xtrafondos.com/wallpapers/resoluciones/19/explosion-polvo-y-humo-de-colores_3840x2160_3008.jpg'} alt='Explosion' ></img>
         </div>
     )
