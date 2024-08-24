6ff3f82e8b190d90a8eaddce485e41cf8190494f
Gianfranco Gobbi <93214155+GianfrancoGobbi@users.noreply.github.com>
2024-08-24 15:31:29 -0300

    Update Home.jsx

--- proyecto/src/Components/Home/Home.jsx
+++ proyecto/src/Components/Home/Home.jsx
@@ -132,7 +132,12 @@ export function Home() {
     return (
         <div className='home'>
             {loader && <span className="loader"></span>}
-            <iframe src='https://my.spline.design/untitled-4fc8777f8656224d3de77260eacc311e/' frameborder='0' width='100%' height='100%'></iframe>
+             <iframe 
+            src='https://my.spline.design/untitled-4fc8777f8656224d3de77260eacc311e/' 
+            frameBorder='0' 
+            width='100%' 
+            height='100%'
+        ></iframe>
             //<Spline scene="https://prod.spline.design/MI2cp74rgIcQHatq/scene.splinecode" />
         </div>
         
